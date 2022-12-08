f905fb74266a2ad7db674439a2d090f161908a33
evan <evan.hoizey@ynov.com>
2022-12-08 11:08:34 +0100

    login page done

--- app/src/main/java/com/example/cookingmama/LoginActivity.kt
+++ app/src/main/java/com/example/cookingmama/LoginActivity.kt
@@ -4,17 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        var editTextTextEmailAddress = findViewById(R.id.editTextTextEmailAddress) as EditText
+        var editTextTextPassword = findViewById(R.id.editTextTextPassword) as EditText
+        var btn_submit = findViewById(R.id.login_button) as Button
 
-        val buttonClick = findViewById<Button>(R.id.login_button)
-        buttonClick.setOnClickListener {
-            val intent = Intent(this, ListingActivity::class.java)
-            startActivity(intent)
+        btn_submit.setOnClickListener {
+            val email = editTextTextEmailAddress.text;
+            val password = editTextTextPassword.text;
+            if(email.length > 5 && password.length > 8) {
+                val intent = Intent(this, ListingActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
--- app/src/main/java/com/example/cookingtables/ListingActivity.kt
+++ app/src/main/java/com/example/cookingtables/ListingActivity.kt
@@ -2,8 +2,14 @@ package com.example.cookingmama
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
 import java.io.IOException
 
 class ListingActivity : AppCompatActivity()
@@ -11,15 +17,55 @@ class ListingActivity : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listing)
+
     }
 
-    val client = OkHttpClient()
+    object Recipes {
+        private const val BASE_URL: String = "https://api.spoonacular.com/recipes/random?number=30"
 
-    val request = Request.Builder()
-        .url("https://api.spoonacular.com/recipes/random?number=30")
-        .get()
-        .build()
+        private val gson : Gson by lazy {
+            GsonBuilder().setLenient().create()
+        }
 
-    val response = client.newCall(request).execute()
+        private val httpClient : OkHttpClient by lazy {
+            OkHttpClient.Builder().build()
+        }
 
+        private val retrofit : Retrofit by lazy {
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(httpClient)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+        }
+
+        val apiService :  ApiService by lazy{
+            retrofit.create(ApiService::class.java)
+        }
+    }
+
+    interface ApiService {
+        @GET("recipes")
+        suspend fun getRecipes(): Response<MutableList<Recipe>>
+    }
+
+    data class Recipe(
+        val id: Int? = null,
+        val title: String? = null,
+        val description: String? = null
+    )
+
+        val client = OkHttpClient()
+
+    /**
+     * Envoit une requête à une API renseignée.
+     *
+     * @param String
+     * @return API Request
+     */
+    private fun execute(url: String) {
+        val request = Request.Builder()
+            .url(url)
+            .build()
+    }
 }
